7a0a4619c94a2edd070240c71bf4d6e611447cdf
RickN123 <nicolai.richard@gmail.com>
2019-05-06 23:43:52 -0400

    sample burger set

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger Special', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Macintosh Burger', true);
