ba31f6e3eff75f3f57e8730ae0e0ae13542e8d34
RickN123 <nicolai.richard@gmail.com>
2019-05-06 21:57:23 -0400

    controller update

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -44,7 +44,17 @@ router.put("/api/burgers", function (req, res) {
 });
 
 router.delete("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
 
+    burger.delete(condition, function (result) {
+        if (result.affectedRows === 0) {
+
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 
 }
 
