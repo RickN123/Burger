7782b6a04bc3393f9008b3c6619446184227d740
RickN123 <nicolai.richard@gmail.com>
2019-05-06 21:26:12 -0400

    route build

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,3 +3,16 @@ var express = require("express");
 var router = express.Router();
 
 var burger = require("../models/burger.js");
+
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        var hbsObject = {
+            burgers: data
+
+        };
+        console.log(hbsObject);
+
+    });
+});
+
+router.post("")
