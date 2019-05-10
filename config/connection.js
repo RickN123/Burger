e9fc36406a598415ad73846325a19e61b9fa851a
RickN123 <nicolai.richard@gmail.com>
2019-05-09 22:38:18 -0400

    updated heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,29 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "axxb6a0z2kydkco3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    port: 3306,
-    user: "e7v1fy119a5ci8ue",
-    password: "uekieji73y8zz7oi",
-    database: "a1h5w64r5selidop"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+        port: 3306,
+        host: "axxb6a0z2kydkco3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        user: "e7v1fy119a5ci8ue",
+        password: "uekieji73y8zz7oi",
+        database: "a1h5w64r5selidop"
+    })
+};
+
+
+
+// var connection = mysql.createConnection({
+//     host: "axxb6a0z2kydkco3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//     port: 3306,
+//     user: "e7v1fy119a5ci8ue",
+//     password: "uekieji73y8zz7oi",
+//     database: "a1h5w64r5selidop"
+// });
 
 connection.connect(function (err) {
     if (err) {
